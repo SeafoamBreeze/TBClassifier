f6f702424373125800c33adfd2260395ae9688db
SeafoamBreeze <zhenghui6696@gmail.com>
2026-03-25 14:29:27 +0800

    Modified test metrics

--- neural_network/densenet_classifier.py
+++ neural_network/densenet_classifier.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 from torchvision import models
 from sklearn.metrics import precision_score, recall_score, fbeta_score, matthews_corrcoef
 
-
 class DenseNetClassifier(pl.LightningModule):
+
     def __init__(self, learning_rate, dropout, weight_decay):
 
         super().__init__()
@@ -26,13 +26,18 @@ class DenseNetClassifier(pl.LightningModule):
         class_weights = torch.tensor([1.00, 1.00, 4.75])
         self.register_buffer('class_weights', class_weights)
 
+        self.train_accs = []
+        self.train_losses = []
         self.val_preds = []
         self.val_targets = []
+        self.test_preds = []
+        self.test_targets = []
 
     def forward(self, x):
         return self.backbone(x)
 
     def training_step(self, batch, batch_idx):
+
         x, y = batch
         logits = self(x)
 
@@ -40,12 +45,24 @@ class DenseNetClassifier(pl.LightningModule):
         acc = (preds == y).float().mean()
         loss = F.cross_entropy(logits, y, weight=self.class_weights)
 
-        self.log('train_acc', acc, on_step=False, on_epoch=True, prog_bar=True)
-        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.train_accs.append(acc.detach())
+        self.train_losses.append(loss.detach())
 
         return loss
 
+    def on_train_epoch_end(self):
+
+        avg_acc = torch.stack(self.train_accs).mean()
+        avg_loss = torch.stack(self.train_losses).mean()
+        
+        self.log('train_acc', avg_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train_loss', avg_loss, on_step=False, on_epoch=True,prog_bar=True)
+
+        self.train_accs.clear()
+        self.train_losses.clear()
+
     def validation_step(self, batch, batch_idx):
+
         x, y = batch
         logits = self(x)
 
@@ -61,16 +78,21 @@ class DenseNetClassifier(pl.LightningModule):
 
     def on_validation_epoch_end(self):
 
-        preds = torch.cat(self.val_preds)
-        targets = torch.cat(self.val_targets)
+        all_preds  = torch.cat(self.val_preds)
+        all_targets  = torch.cat(self.val_targets)
 
-        mcc = matthews_corrcoef(targets.numpy(), preds.numpy())
+        try:
+            mcc = matthews_corrcoef(all_targets.numpy(), all_preds.numpy())
+        except ValueError:
+            mcc = 0.0
+
         self.log("val_mcc", mcc, prog_bar=True)
 
         self.val_preds.clear()
         self.val_targets.clear()
 
     def test_step(self, batch, batch_idx):
+
         x, y = batch
         logits = self(x)
 
@@ -78,32 +100,32 @@ class DenseNetClassifier(pl.LightningModule):
         acc = (preds == y).float().mean()
         loss = F.cross_entropy(logits, y, weight=self.class_weights)
 
-        # TODO: Look into torchmetrics.classification
-        # Numpy requires CPU so either use the above library or move it to on_epoch_end
+        self.log('test_acc', acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        
+        self.test_preds.append(preds.detach().cpu())
+        self.test_targets.append(y.detach().cpu())
+
+    def on_test_epoch_end(self):
+
+        all_preds = torch.cat(self.test_preds)
+        all_targets = torch.cat(self.test_targets)
+        y_pred = all_preds.numpy()
+        y_true = all_targets.numpy()
         tb_class = 2
-        tb_precision = precision_score(y.cpu().numpy(), preds.cpu().numpy(), labels=[tb_class], average="macro", zero_division=0)
-        tb_recall = recall_score(y.cpu().numpy(), preds.cpu().numpy(), labels=[tb_class], average="macro", zero_division=0)
-        tb_f2 = fbeta_score(y.cpu().numpy(), preds.cpu().numpy(), beta=2, labels=[tb_class], average="macro", zero_division=0)
-        try:
-            mcc = matthews_corrcoef(y.cpu().numpy(), preds.cpu().numpy())
-        except Exception:
-            mcc = 0.0
-
-        self.log("test_loss", loss)
-        self.log("test_acc", acc)
-        self.log("test_TB_precision", tb_precision)
-        self.log("test_TB_recall", tb_recall)
-        self.log("test_TB_f2", tb_f2)
-        self.log("test_mcc", mcc)
-
-        return {
-            "test_loss": loss.detach(),
-            "test_acc": acc.detach(),
-            "test_TB_precision": torch.tensor(tb_precision, device=y.device),
-            "test_TB_recall": torch.tensor(tb_recall, device=y.device),
-            "test_TB_f2": torch.tensor(tb_f2, device=y.device),
-            "test_mcc": torch.tensor(mcc, device=y.device)
+        
+        metrics = {
+            'test_TB_precision': precision_score(y_true, y_pred, labels=[tb_class], average=None)[0],
+            'test_TB_recall': recall_score(y_true, y_pred, labels=[tb_class], average=None)[0],
+            'test_TB_f2': fbeta_score(y_true, y_pred, beta=2, labels=[tb_class], average=None)[0],
+            'test_mcc': matthews_corrcoef(y_true, y_pred),
         }
+        
+        for name, value in metrics.items():
+            self.log(name, value, prog_bar=True)
+        
+        self.test_preds.clear()
+        self.test_targets.clear()
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
